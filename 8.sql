df2ca381cf4d61517c23c84bea3e88ca67870c42
4d4m43 <adam.abdelmalek@gmail.com>
2019-02-07 11:38:34 -0600

    8

--- 8.sql
+++ 8.sql
@@ -1,36 +1,23 @@
 USE sakila;
 
--- 6a --
-SELECT s.first_name, s.last_name, a.address
-FROM staff as s
-JOIN address as a
-ON s.address_id = a.address_id;
+-- 8a --
+CREATE VIEW top_5 AS
+SELECT c.name, SUM(p.amount)
+FROM category c
+JOIN film_category AS fc
+ON c.category_id = fc.category_id
+JOIN inventory AS i
+ON i.film_id = fc.film_id
+JOIN rental AS r
+ON r.inventory_id = i.inventory_id
+JOIN payment AS p
+ON p.rental_id = r.rental_id
+GROUP BY c.name
+ORDER BY SUM(p.amount) DESC
+LIMIT 5;
 
--- 6b --
-SELECT s.first_name, s.last_name, SUM(p.amount)
-FROM staff as s
-JOIN payment as p
-ON s.staff_id = p.staff_id
-WHERE MONTH(payment_date) = 8 and YEAR(payment_date) = 2005;
+-- 8b --
+SELECT * FROM top_5;
 
--- 6c --
-SELECT f.title, fa.actor_id
-FROM film as f
-JOIN film_actor as fa
-ON f.film_id = fa.film_id
-GROUP BY f.film_id;
-
--- 6d --
-SELECT f.title, COUNT(inventory_id)
-FROM film as f
-JOIN inventory as i
-ON f.film_id = i.film_id
-WHERE f.title = 'Hunchback Impossible';
-
--- 6e --
-SELECT c.first_name, c.last_name, SUM(p.amount)
-FROM customer as c
-JOIN payment as p
-ON c.customer_id = p.customer_id
-GROUP BY c.customer_id
-ORDER BY last_name ASC;
+-- 8c --
+DROP VIEW top_5;
