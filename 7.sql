0e456064e50e54ded1bcfb1fcb45a4cb88c7fac9
4d4m43 <adam.abdelmalek@gmail.com>
2019-02-06 21:28:32 -0600

    sql

--- 7.sql
+++ 7.sql
@@ -1,35 +1,52 @@
 USE sakila;
 
--- 6a --
-SELECT s.first_name, s.last_name, a.address
-FROM staff as s
-JOIN address as a
-ON s.address_id = a.address_id;
+-- 7a --
+SELECT *
+FROM film f 
+WHERE (f.title LIKE 'K%') OR (f.title LIKE 'Q%') 
+AND f.title IN(
+	SELECT title
+    FROM film ff
+    where ff.language_id = '1'
+    );
 
--- 6b --
-SELECT s.first_name, s.last_name, SUM(p.amount)
-FROM staff as s
-JOIN payment as p
-ON s.staff_id = p.staff_id
-WHERE MONTH(payment_date) = 8 and YEAR(payment_date) = 2005;
+-- 7b --
+SELECT *
+FROM actor a 
+WHERE a.actor_id IN(
+	SELECT actor_id
+    FROM film_actor fa
+    where fa.film_id IN(
+		SELECT film_id
+        FROM film f
+        WHERE f.title = 'Alone Trip'
+    )
+);
 
--- 6c --
-SELECT f.title, fa.actor_id
-FROM film as f
-JOIN film_actor as fa
-ON f.film_id = fa.film_id
-GROUP BY f.film_id;
-
--- 6d --
-SELECT f.title, COUNT(inventory_id)
-FROM film as f
-JOIN inventory as i
-ON f.film_id = i.film_id
-WHERE f.title = 'Hunchback Impossible';
-
--- 6e --
-SELECT c.first_name, c.last_name, SUM(p.amount)
+-- 7c --
+SELECT c.first_name, c.last_name, c.email, c.address_id
 FROM customer as c
+JOIN address as a
+ON c.address_id = a.address_id
+JOIN city as cy
+ON a.city_id = cy.city_id
+JOIN country as cu
+ON cu.country_id = cy.country_id
+WHERE cu.country = 'Canada'
+ORDER BY last_name ASC;
+
+-- 7d --
+SELECT f.title, fc.category_id, c.name
+FROM film as f
+JOIN film_category as fc
+ON f.film_id = fc.film_id
+JOIN category as c
+ON fc.category_id = c.category_id
+WHERE c.name = 'Family';
+
+-- 7e --
+SELECT r.inventory_id, COUNT(c.last_name, SUM(p.amount)
+FROM rental as r
 JOIN payment as p
 ON c.customer_id = p.customer_id
 GROUP BY c.customer_id
